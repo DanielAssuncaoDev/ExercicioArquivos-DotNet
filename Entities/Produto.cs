e798480516805b5b029fe72867e710b45e76095a
DanielAssuncaoDev <danielaristotelis@gmail.com>
2022-02-26 14:18:24 -0300

    Exercicio de Arquivos Finalizado

---
+++ Entities/Produto.cs
@@ -0,0 +1,42 @@
+﻿namespace Exerc_Arquivos
+{
+    class Produto
+    {
+        public string Descricao { get; private set; }
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        public Produto() { }
+        public Produto(string descricao, double preco, int quantidade)
+        {
+            Descricao = descricao;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        public Produto(string produtoInCSV)
+        {
+            TransformCSVInProduto(produtoInCSV);
+        }
+
+
+        public double CalcularTotal()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void TransformCSVInProduto(string produto)
+        {
+            string[] atributos = produto.Split(',');
+            Descricao = atributos[0];
+            Preco = double.Parse(atributos[1]);
+            Quantidade = int.Parse(atributos[2]);
+        }
+
+        public string TransformProdutoInCSV()
+        {
+            return $"{Descricao},{CalcularTotal()}";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exerc_Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o Path do arquivo .CSV: ");
+            string path = Console.ReadLine();
+            while (Path.GetExtension(path) != ".csv")
+            {
+                Console.WriteLine("Insira o Path do arquivo .CSV: ");
+                path = Console.ReadLine();
+            }
+            try
+            {
+                string newDirectory = Path.GetDirectoryName(path) + @"\out";
+                Directory.CreateDirectory(newDirectory);
+                
+
+                using (FileStream file = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader readerFile = new StreamReader(file) )
+                    {
+                        using (
+                            StreamWriter writerFile = 
+                                new StreamWriter(
+                                    new FileStream(newDirectory + @"\summary.csv", FileMode.Create)
+                                )
+                            )
+                        {
+                            // Lê as linhas do arquivo .CSV, os transforma em uma instância de Produto
+                            while (!readerFile.EndOfStream)
+                            {
+                                string line = readerFile.ReadLine();
+                                Produto produto = new Produto(line);
+                                Console.WriteLine(produto.TransformProdutoInCSV());
+                                writerFile.WriteLine(produto.TransformProdutoInCSV());
+                            }
+                        }  
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
